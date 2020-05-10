2a9efa1732c89b08dc5cf0c5b5a45359b085d39f
karangupta77 <61135125+karangupta77@users.noreply.github.com>
2020-05-10 22:54:52 +0530

    second commit

--- src/main/java/com/cg/onlinewallet/entities/WalletAccount.java
+++ src/main/java/com/cg/onlinewallet/entities/WalletAccount.java
@@ -1,5 +1,73 @@
+/*
+* Created by:Satya Swagatam Panda
+* Description: This is an entity class used to define variables that are going to be used in the application.
+*              This also contains getter and setter methods for retrieving and updating the values of the variables.
+*/
 package com.cg.onlinewallet.entities;
 
-public class WalletAccount {
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "UserAccount")
+public class WalletAccount implements Serializable {
+	@Id
+	@GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "acc_seq")
+	private Integer accountID;
+	@Column(name="balance",precision=2)
+	private Double accountBalance;
+
+	public enum status {
+		active, non_active
+	};
+
+	@Enumerated(EnumType.STRING)
+	@Column(name="user_status")
+	private status userStatus = status.non_active;
+
+	@OneToMany(cascade = CascadeType.ALL)
+	private List<WalletTransactions> transactionList;
+
+	public Integer getAccountID() {
+		return accountID;
+	}
+
+	public Double getAccountBalance() {
+		return accountBalance;
+	}
+
+	public void setAccountBalance(Double accountBalance) {
+		this.accountBalance = accountBalance;
+	}
+
+	public WalletAccount(Double accountBalance, List<WalletTransactions> transactionList, status userStatus) {
+		super();
+
+		this.accountBalance = accountBalance;
+		this.transactionList = transactionList;
+		this.userStatus = userStatus;
+	}
+
+	public status getUserStatus() {
+		return userStatus;
+	}
+
+	public void setUserStatus(status userStatus) {
+		this.userStatus = userStatus;
+	}
+
+	public List<WalletTransactions> getTransactionList() {
+		return transactionList;
+	}
+
+	public void setTransactionList(List<WalletTransactions> transactionList) {
+		this.transactionList = transactionList;
+	}
+
+	public WalletAccount() {
+		// TODO Auto-generated constructor stub
+	}
 
 }
